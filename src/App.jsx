3116389b1dff9422ee8aabf0d35ce2414412a77e
Taha Ghous <153850706+tahaghous16@users.noreply.github.com>
2025-05-17 20:46:23 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ const App = () => {
   const [classes, setClasses] = useState([]);
 
   useEffect(() => {
-    fetch("https://sms.ilmwasooli.com/temp/gettestingdata")
+    fetch("/api/temp/gettestingdata")
       .then((response) => response.json())
       .then((json) => {
         setData(json.Data);
